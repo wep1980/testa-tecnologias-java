9683a6715969adc4d91193d048b2740a59b4b561
m390443 <waldir.pereira@7comm.com.br>
2023-09-25 14:29:47 -0300

    Criado o projeto rh-trabalhador

---
+++ rh-trabalhador/src/main/java/br/com/wepdev/trabalhador/RhTrabalhadorApplication.java
@@ -0,0 +1,13 @@
+package br.com.wepdev.trabalhador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhTrabalhadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhTrabalhadorApplication.class, args);
+	}
+
+}
---
+++ rh-trabalhador/src/test/java/br/com/wepdev/trabalhador/RhTrabalhadorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wepdev.trabalhador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhTrabalhadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
